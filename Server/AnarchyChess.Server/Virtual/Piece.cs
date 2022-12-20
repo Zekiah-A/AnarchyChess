c039aa803ee6209fa95914388330875035c48bac
Zekiah-A <zekiahamoako@outlook.com>
2022-12-20 16:06:11 +0000

    Start adding bishop and queen valid moves

--- Server/AnarchyChess.Server/Virtual/Piece.cs
+++ Server/AnarchyChess.Server/Virtual/Piece.cs
@@ -4,4 +4,4 @@ namespace AnarchyChess.Server.Virtual;
 
 public sealed record Piece(string Token, PieceType Type, PieceColour Colour, Board Board);
 // TODO: Holding a board instance in piece mem violates tree direction, and creates circular reference (memory leak).
-// TODO: Instead, make each element in the tree know it's column and row in the parent's array.
+// TODO: Instead, make each element in the tree know it's column and row in the parent's array.    
